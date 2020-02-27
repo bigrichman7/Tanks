e5cfb381d03f831e6c57c4db2dcc65d05a741def
bigrichman7 <bigrichman7@gmail.com>
2020-02-27 22:16:43 +0300

    до 6 урока

--- src/main/java/org/bigrichman7/display/Display.java
+++ src/main/java/org/bigrichman7/display/Display.java
@@ -2,6 +2,10 @@ package org.bigrichman7.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public abstract class Display {
 
@@ -9,36 +13,58 @@ public abstract class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void create(int width, int height, String title){
+    private static BufferedImage buffer;
+    private static int[] bufferData;
+    private static Graphics bufferGraphics;
+    private static int clearColor;
+
+    private static BufferStrategy bufferStrategy;
+
+    private static float delta = 0;
+
+    public static void create(int width, int height, String title, int _clearColor, int numBuffers){
         if (!created) {
             window = new JFrame(title);
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            content = new Canvas(){
-                @Override
-                public void paint(Graphics g) {
-                    super.paint(g);
-                    render(g);
-                }
-            };
+            content = new Canvas();
 
             Dimension size = new Dimension(width, height);
             content.setPreferredSize(size);
-            content.setBackground(Color.black);
 
             window.setResizable(false);
             window.getContentPane().add(content);
             window.pack();
             window.setLocationRelativeTo(null);
             window.setVisible(true);
-        }
+
+            buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            bufferData = ((DataBufferInt)buffer.getRaster().getDataBuffer()).getData();
+            bufferGraphics = buffer.getGraphics();
+            clearColor = _clearColor;
+
+            content.createBufferStrategy(numBuffers);
+            bufferStrategy = content.getBufferStrategy();
+        } else
+            created = true;
+    }
+
+    public static void clear(){
+        Arrays.fill(bufferData, clearColor);
     }
 
     public static void render(){
-        content.repaint();
+        bufferGraphics.setColor(new Color(0xff0000ff));
+        bufferGraphics.fillOval((int)(350 + (Math.sin(delta) * 200)), 250, 100, 100);
+
+        ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        bufferGraphics.fillOval((int)(500 + (Math.sin(delta) * 200)), 250, 100, 100);
+        ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+        //delta += 0.02f;
     }
 
-    private static void render(Graphics g){
-        g.setColor(Color.white);
-        g.fillOval(400-50, 300-50, 100, 100);
+    public static void swapBuffers(){
+        Graphics g = bufferStrategy.getDrawGraphics();
+        g.drawImage(buffer, 0, 0, null);
+        bufferStrategy.show();
     }
 }
--- src/main/java/org/bigrichman7/main/Main.java
+++ src/main/java/org/bigrichman7/main/Main.java
@@ -7,11 +7,13 @@ import java.awt.event.ActionEvent;
 
 public class Main {
     public static void main(String[] args) {
-        Display.create(800, 600, "Tanks");
+        Display.create(800, 600, "Tanks", 0xff00ff00, 3);
 
         Timer timer = new Timer(1000/60, new AbstractAction(){
             public void actionPerformed(ActionEvent e) {
+                Display.clear();
                 Display.render();
+                Display.swapBuffers();
             }
         });
 
